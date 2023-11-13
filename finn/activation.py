fixed negative derivatives (integral +C term)

finn/activation.py
@@ -37,7 +37,10 @@ class IntegralActivation(torch.nn.Module):
 		x_ = sympy.Symbol('x')
 		erfi = (sympy.functions.special.error_functions.erf(x_) + 1) / 2
 		for i in range(2, self.n+1):
+			print(i)
 			erfi = sympy.integrate(erfi, x_)
+			c_offset = sympy.limit(erfi, x_, -float('inf'))
+			erfi = erfi - c_offset
 			erfi_simp = erfi.simplify()
 			acti = sympytorch.SymPyModule(expressions=[erfi_simp], extra_funcs={sympy.core.numbers.Pi: lambda: torch.pi})
 			acts[i] = squeeze_output(acti)
@@ -66,7 +69,7 @@ class IntegralActivation(torch.nn.Module):
 						# assert dx.shape == real_dx.shape
 						# assert (dx-real_dx).norm() == 0
 					else:
-						dx = deriv_mod(x)
+						dx = deriv_mod(x).clone()
 						IntAct.backward_vals[i] = dx
 					return grad_output * dx
 			return IntAct

finn/mlp.py
@@ -51,7 +51,7 @@ class IntegralNetwork(nn.Module):
 			for layer in self.nets[i].net:
 				if isinstance(layer, IntegralActivation):
 					self.acts.append(layer)
-		# self.init_weights()
+		self.init_weights()
 
 	def forward(self, x):
 		return torch.cat([net(x.double()).type(x.dtype) for net in self.nets], dim=-1).sum(dim=-1).unsqueeze(-1)
@@ -65,7 +65,8 @@ class IntegralNetwork(nn.Module):
 		def helper(m):
 			if isinstance(m, nn.Linear):
 				torch.nn.init.kaiming_normal_(m.weight)
-				m.bias.data.fill_(0)
+				m.weight.data = m.weight / 2
+				# m.bias.data.fill_(0)
 		self.nets.apply(helper)
 
 
@@ -103,4 +104,5 @@ class LinearAbs(nn.Linear):
 		super().__init__(*args, **kwargs)
 
 	def forward(self, input: Tensor) -> Tensor:
-		return nn.functional.linear(input, torch.abs(self.weight), self.bias)
+		out = nn.functional.linear(input, torch.abs(self.weight), self.bias)
+		return out

run/test_nan.py
@@ -1,8 +1,9 @@
 import torch
 from finn import Finn
+from matplotlib import pyplot as plt
 
 def run():
-        torch.manual_seed(3)
+        # torch.manual_seed(3)
 
         x = torch.tensor([[0.2445, 1.7579, -0.4431, 0.8229],
                 [-0.1701, 1.5750, -0.8577, 0.6400],
@@ -142,7 +143,13 @@ def run():
         )
 
         y = f(x)
+
         print(y)
+
+        # Y = f.F(x)
+        # for i in range(x.shape[-1]):
+        #         plt.scatter(x[:,i].detach(), y.detach())
+        # plt.show()
 
 if __name__ == "__main__":
         run()